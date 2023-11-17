f06b3315b7081603094d499eb68d408d8cdea18b
Madkhix <62099980+Madkhix@users.noreply.github.com>
2023-11-17 09:52:13 +0300

    Update Hough Transform.py

--- Hough Transform.py
+++ Hough Transform.py
@@ -32,7 +32,7 @@ masked_edges = cv2.bitwise_and(edges, mask)
 # Make a blank the same size as our image to draw on
 rho = 1 # Distance resolution in pixels of the Hough grid
 theta = np.pi/180 # Angular resolution in radians of the Hough grid
-threshold = 2     # minimum number of votes (intersections in Hough grid cell)
+threshold = 2     # Minimum number of votes (intersections in Hough grid cell)
 min_line_length = 4 #minimum number of pixels making up a line
 max_line_gap = 5    # maximum gap in pixels between connectable line segments
 line_image = np.copy(image)*0 # creating a blank to draw lines on

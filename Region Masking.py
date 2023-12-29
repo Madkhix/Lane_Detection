5e0c212fbc2374b2e50a1956b5576052211c24b9
Madkhix <62099980+Madkhix@users.noreply.github.com>
2023-12-29 10:25:07 +0300

    Update Region Masking.py

--- Region Masking.py
+++ Region Masking.py
@@ -14,7 +14,7 @@ color_select = np.copy(image)
 line_image = np.copy(image)
 
 # Define color selection criteria
-# Modify these variables to make your COLOR SELECTION
+# Modify these variables to make your color SELECTION
 red_threshold = 200
 green_threshold = 200
 blue_threshold = 200
